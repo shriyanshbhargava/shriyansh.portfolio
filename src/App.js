3b852b41c5ecc78fff0769a8a716030810a54fcf
Shriyansh Bhargava <63575417+shriyanshbhargava@users.noreply.github.com>
2024-02-24 15:55:41 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   React.useEffect(() => {
     const timer = setTimeout(() => {
       upadateLoad(false);
-    }, 3000);
+    }, 1000);
     return () => clearTimeout(timer);
   }, []);
 
