296d86d4ffca7b4f4cb836bb849b3c24f5645cd5
Shriyansh Bhargava <63575417+shriyanshbhargava@users.noreply.github.com>
2024-03-10 11:53:26 +0530

    added new project

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,49 +4,58 @@ import Section from "./Section/Section";
 import { Folder, Link } from "../assets/icons";
 export default function Projects() {
   const projects = [
-    {
-      id: 0,
-      title: "ToDo With MERN",
-      link: "https://github.com/shriyanshbhargava/todo-mern",
-      desc: "A concise To-Do app built with MERN stack, featuring CRUD operations for task management.      ",
-      tech: ["MongoDB / ExpressJS / React / NodeJS / Tailwind CSS"],
-    },
+    // {
+    //   id: 0,
+    //   title: "Component Library",
+    //   link: "https://github.com/shriyanshbhargava/component-library",
+    //   desc: "Made A Small Component Library for my personal use.      ",
+    //   tech: ["JavaScript / HTML / CSS"],
+    // },
     {
       id: 1,
-      title: "Yuvraj Crackers",
-      link: "https://yuvraj-repo-2.vercel.app/",
-      desc: "A Freelance Ecommerce Project App with Product Listing & Admin Panel to Add/Modify Orders/Customers/Products.      ",
-      tech: ["JavaScript / React / React Router / Tailwind CSS"],
-    },
-    {
-      id: 2,
-      title: "Notion CLone",
-      link: "https://notion-clone-lilac.vercel.app/",
-      desc: "I was tasked with creating a website that functions as a clone of Notion.",
-      tech: ["React / Tailwind CSS"],
-    },
-    {
-      id: 3,
-      title: "Ecommerce-Clone",
-      link: "https://github.com/shriyanshbhargava/ecommerce-clone",
-      desc: "A Ecommerce React App with Features like Sign In/Register, Cart & Filter",
-      tech: ["JavaScript / React / React Router / Tailwind CSS"],
-    },
-    {
-      id: 4,
       title: "The Learnest",
       link: "https://github.com/shriyanshbhargava/TheLearnest",
       desc: "The website served as a promotional platform, redirecting Instagram traffic to the website for further engagement.",
       tech: ["JavaScript / HTML / CSS"],
     },
     {
+      id: 2,
+      title: "Ecommerce-Clone",
+      link: "https://github.com/shriyanshbhargava/ecommerce-clone",
+      desc: "A Ecommerce React App with Features like Sign In/Register, Cart & Filter",
+      tech: ["JavaScript / React / React Router / Tailwind CSS"],
+    },
+    {
+      id: 3,
+      title: "Notion CLone",
+      link: "https://notion-clone-lilac.vercel.app/",
+      desc: "I was tasked with creating a website that functions as a clone of Notion.",
+      tech: ["React / Tailwind CSS"],
+    },
+    {
+      id: 4,
+      title: "Yuvraj Crackers",
+      link: "https://yuvraj-repo-2.vercel.app/",
+      desc: "A Freelance Ecommerce Project App with Product Listing & Admin Panel to Add/Modify Orders/Customers/Products.      ",
+      tech: ["JavaScript / React / React Router / Tailwind CSS"],
+    },
+    {
       id: 5,
-      title: "Component Library",
-      link: "https://github.com/shriyanshbhargava/component-library",
-      desc: "Made A Small Component Library for my personal use.      ",
-      tech: ["JavaScript / HTML / CSS"],
+      title: "ToDo With MERN",
+      link: "https://github.com/shriyanshbhargava/todo-mern",
+      desc: "A concise To-Do app built with MERN stack, featuring CRUD operations for task management.      ",
+      tech: ["MongoDB / ExpressJS / React / NodeJS / Tailwind CSS"],
+    },
+    {
+      id: 6,
+      title: "LinkInBio (Underway)",
+      link: "https://github.com/shriyanshbhargava/linkinbio-Frontend",
+      desc: "Everything you are. In one, simple link in bio.      ",
+      tech: ["MongoDB / ExpressJS / React / NodeJS / Tailwind CSS"],
     },
   ];
+
+  const reversedArray = projects.slice().reverse();
 
   return (
     <Section
@@ -55,7 +64,7 @@ export default function Projects() {
       title={data.projects.projectsTitle}
     >
       <div className="grid lg:grid-cols-1 md:grid-cols-2 gap-10 text-md leading-6 text-slate-400">
-        {projects.map((project) => {
+        {reversedArray.map((project) => {
           return (
             <div
               className="grid gap-7 p-6 bg-[#C5DFDE] shadow-lg rounded-lg"
